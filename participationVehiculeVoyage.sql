cf70c5453534a1570af082effb855be41c14c5db
Michellah <hei.michellah@gmail.com>
2022-06-21 18:43:21 +0200

    remove participationVehiculeClient.sql

--- participationVehiculeVoyage.sql
+++
@@ -1,6 +0,0 @@
---tous les vehicule qui ont participer à un voyage à une date précise
-
-select matricule,date_voyage,nom_ville 
-    from voyage 
-    inner join ville 
-    on voyage.id_voyage=ville.id_ville;
